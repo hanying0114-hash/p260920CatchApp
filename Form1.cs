bb24cc54a50a89f84b3c75998c6321c178cf2a30
知穎 韓 <hanying0114@gmail.com>
2026-09-25 16:41:15 +0800

    加入專案檔案。

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+using System.Runtime.InteropServices;   //[DllImport]
+using System.Text;  //StringBuilder
+namespace p260920CatchApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]   //Dll = Dynamic Link Library 動態連結程式庫
+        private static extern IntPtr GetForegroundWindow(); 
+        //IntPtr = pointer to an integer (指向整數的指標) 抓桌面身分證進來
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder appName, int size);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            catchAPP.Enabled = true;
+        }
+        private void catchAPP_Tick_1(object sender, EventArgs e)
+        {
+            IntPtr hWnd = GetForegroundWindow();    
+            //hWnd = 前景視窗
+            StringBuilder appName = new StringBuilder(256); 
+            //appName = 新的字串建構器，容量256
+            GetWindowText(hWnd, appName, appName.Capacity); 
+            //根據hWnd抓取前景視窗的標題文字，放入appName
+            label1.Text += $"{appName.ToString()}\n";
+        }
+    }
+}
